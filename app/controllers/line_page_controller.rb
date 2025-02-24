a33f6d7c60db5bb534a14c49b072af1ecc6948b4
kai1117-open <180093236+kai1117-open@users.noreply.github.com>
2025-02-24 11:23:10 +0000

    linepageの作成

--- app/controllers/line_page_controller.rb
+++ app/controllers/line_page_controller.rb
@@ -2,4 +2,13 @@ class LinePageController < ApplicationController
   def top
     
   end
+  def group
+
+  end
+  def hayato
+
+  end
+  def keisuke
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   get 'broken_smartphone/top'
 
-  get 'line_page/top'
+  get 'line_page/top',    to: 'line_page#top',    as: 'line_page_top'
+  get 'line_page/group',  to: 'line_page#group',  as: 'line_page_group'
+  get 'line_page/hayato', to: 'line_page#hayato', as: 'line_page_hayato'
+  get 'line_page/keisuke',to: 'line_page#keisuke',as: 'line_page_keisuke'
 
   get 'google_page/top'
 
