754a2015a93fb3900bc3c0d0777e4b6d8607279a
kai1117-open <180093236+kai1117-open@users.noreply.github.com>
2025-02-27 11:27:10 +0000

    進捗レベル4まで仕組みを作成

--- app/controllers/line_page_controller.rb
+++ app/controllers/line_page_controller.rb
@@ -5,18 +5,35 @@ class LinePageController < ApplicationController
 
   def group
     if request.get?
-
+      # GETリクエストの処理をここに書く
     elsif request.post?
       if current_user.progress_level == 2
         group = current_user.line_group
         group.update(chat_1: params[:chat_1])
-        if current_user.line_group.chat_1&.include?("メール") || current_user.line_group.chat_1&.include?("写真")
+        # chat_1の文字列に「米松」または「茉莉」が含まれているかを判定
+        if current_user.line_group.chat_1&.include?("米松") || current_user.line_group.chat_1&.include?("茉莉")
           current_user.update(progress_level: 3)
         else
-          flash[:alert] = "不正解です" 
+          flash[:alert] = "不正解です"
         end
         redirect_to line_page_group_path
+      end
+
+      if current_user.progress_level == 3
+        group = current_user.line_group
+        group.update(chat_2: params[:chat_2])
+  
+        if current_user.line_group.chat_2 == "キャンペル"
+          current_user.update(progress_level: 4)
+        else
+          flash[:alert] = "不正解です"
         end
+        redirect_to line_page_group_path
+      end
+
+
+
+
     end
   end
 
