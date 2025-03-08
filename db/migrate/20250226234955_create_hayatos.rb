24e4b7c30f022af0d15945543838a13e57eca77c
kai1117-open <180093236+kai1117-open@users.noreply.github.com>
2025-03-08 06:17:46 +0000

    カラムの追加

--- db/migrate/20250226234955_create_hayatos.rb
+++ db/migrate/20250226234955_create_hayatos.rb
@@ -2,7 +2,18 @@ class CreateHayatos < ActiveRecord::Migration[6.1]
   def change
     create_table :hayatos do |t|
       t.string :chat_1, null: true
-      
+      t.string :chat_2, null: true
+      t.string :chat_3, null: true
+      t.string :chat_4, null: true
+      t.string :chat_5, null: true
+      t.string :chat_6, null: true
+      t.string :chat_7, null: true
+      t.string :chat_8, null: true
+      t.string :chat_9, null: true
+      t.string :chat_10, null: true
+      t.string :chat_11, null: true
+      t.string :chat_12, null: true
+      t.string :chat_13, null: true
       t.references :user, null: false, foreign_key: true
       t.timestamps
     end
--- db/migrate/20250226235032_create_googles.rb
+++ db/migrate/20250226235032_create_googles.rb
@@ -3,7 +3,15 @@ class CreateGoogles < ActiveRecord::Migration[6.1]
     create_table :googles do |t|
       t.boolean :page_1, default: false, null: false
       t.boolean :page_2, default: false, null: false
-      
+      t.boolean :page_3, default: false, null: false
+      t.boolean :page_4, default: false, null: false
+      t.boolean :page_5, default: false, null: false
+      t.boolean :page_6, default: false, null: false
+      t.boolean :page_7, default: false, null: false
+      t.boolean :page_8, default: false, null: false
+      t.boolean :page_9, default: false, null: false
+      t.boolean :page_10, default: false, null: false
+      t.boolean :page_11, default: false, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
